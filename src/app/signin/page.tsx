d5eb85c6a73825e3b5f75a566157c25a377b6e74
zmoussam <zakariamoussammi87@gmail.com>
2024-08-15 20:39:01 +0100

    add sign in and sign up pages

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,131 +1,118 @@
 "use client";
-import React from "react";
-import Image from "next/image";
-import styles from "./signin.module.css";
-import { useState } from "react";
-import { FaRegEye } from "react-icons/fa6";
-import { FaRegEyeSlash } from "react-icons/fa";
+import Footer from "@/components/footer/Footer";
 import Link from "next/link";
+import React from "react";
+import { useState } from "react";
+import { FaEnvelope, FaLock, FaEye, FaEyeSlash } from "react-icons/fa";
 
 const Signin = () => {
-  const [showPassword, setShowPassword] = useState(false);
+  const [passwordVisible, setPasswordVisible] = useState(false);
+
+  const togglePasswordVisibility = () => {
+    setPasswordVisible(!passwordVisible);
+  };
 
   return (
     <div className="h-screen">
-      <div className="pt-20 h-full">
-        <div className=" h-full px-40 py-20 bg-white flex items-center justify-center text-center">
-          <div className="bg-black h-full w-1/3">form</div>
-        </div>
-        {/* <div className="bg-gradient-to-t from-[#0B405F] to-[#1784C5] w-full h-full text-center flex items-center justify-center">
-            <div>
-              <Image
-                src="/assets/auth/authside.png" // Replace with your logo image path
-                alt="Logo"
-                width={500}
-                height={500}
-                quality={100}
-              />
+      <div className=" h-full">
+        <div className=" h-full px-40 bg-white dark:bg-gray-900 flex items-center justify-center text-center">
+          <div className="  h-full w-1/3 flex flex-col items-center px-10 justify-center gap-8">
+            <div className="text-center">
+              <div className="font-vietnam text-h2 font-bold mb-3">
+                Login to your account
+              </div>
+              <div className="text-paragraph text-black dark:text-gray-300 opacity-50">
+                Welcome back, Select method to login
+              </div>
             </div>
-          </div> */}
+            <button className=" flex w-3/4 items-center justify-center gap-4 px-4 py-2 rounded-[12px] font-medium transition-colors dark:bg-gray-900 dark:text-white dark:hover:bg-gray-800 bg-white text-black hover:bg-gray-100 border border-gray-300">
+              <img
+                src="https://www.svgrepo.com/show/355037/google.svg"
+                alt="Google Icon"
+                className="h-5 w-5"
+              />
+              Sign in with Google
+            </button>
+            <div className="flex w-full  gap-3 items-center justify-between">
+              <div className="border-t w-full border-black dark:border-gray-300 border-opacity-50"></div>
+              <div className="w-full text-sm text-black dark:text-gray-300 opacity-50">
+                or continue with email
+              </div>
+              <div className="border-t w-full border-black dark:border-gray-300 border-opacity-50"></div>
+            </div>
+            <form className="w-3/4">
+              {/* Email Field */}
+              <div className="mb-6">
+                <div className="flex items-center border rounded-[12px] gap-3 shadow-sm py-2 px-4 bg-gray-100 dark:bg-gray-700">
+                  <FaEnvelope className="text-gray-600 dark:text-gray-400 mr-2" />
+                  <input
+                    className="w-full bg-transparent focus:outline-none text-gray-800 dark:text-white"
+                    type="email"
+                    id="email"
+                    placeholder="Enter your email"
+                    required
+                  />
+                </div>
+              </div>
+
+              {/* Password Field */}
+              <div className="mb-4">
+                <div className="flex items-center border rounded-[12px] shadow-sm py-2 px-4 gap-3 bg-gray-100 dark:bg-gray-700">
+                  <FaLock className="text-gray-600 dark:text-gray-400 mr-2" />
+                  <input
+                    className="w-full bg-transparent focus:outline-none text-gray-800 dark:text-white"
+                    type={passwordVisible ? "text" : "password"}
+                    id="password"
+                    placeholder="Enter your password"
+                    required
+                  />
+                  <button
+                    type="button"
+                    onClick={togglePasswordVisibility}
+                    className="ml-2 text-gray-600 dark:text-gray-400 focus:outline-none"
+                  >
+                    {passwordVisible ? <FaEyeSlash /> : <FaEye />}
+                  </button>
+                </div>
+              </div>
+
+              {/* Remember Me Checkbox */}
+              <div className="mb-6 flex w-full justify-between text-sm px-2">
+                <label className="inline-flex items-center">
+                  <input
+                    type="checkbox"
+                    className="form-checkbox h-4 w-4 text-blue-600"
+                  />
+                  <span className="ml-2 text-gray-700 dark:text-gray-300">
+                    Remember me
+                  </span>
+                </label>
+                <div className="text-[#1784C5]">Forgot password?</div>
+              </div>
+
+              {/* Login Button */}
+              <div className="text-center mb-6">
+                <button
+                  type="submit"
+                  className="w-full bg-[#1784C5] hover:bg-[#18567a] text-white font-bold py-2 px-4 rounded-[12px] focus:outline-none focus:shadow-outline"
+                >
+                  Login
+                </button>
+              </div>
+
+              <div>
+                Dont have account ?{" "}
+                <Link href="/signup" className="text-[#1784C5]">
+                  {" "}
+                  Create account
+                </Link>
+              </div>
+            </form>
+          </div>
+        </div>
       </div>
+      <Footer />
     </div>
-    // <div className="container">
-    //   <div className=" bg-gray-200">
-    //     <div className="h-full w-full grid md:grid-cols-2 rounded-md overflow-hidden">
-    //       <div className="right flex flex-col justify-evenly items-center bg-white">
-    //         <div className="text-center py-10"></div>
-    //         <section className="w-7/12 mx-auto flex text-start  items-center flex-col gap-7">
-    //           <div>
-    //             <Image
-    //               src="/assets/logo/logo.png" // Replace with your logo image path
-    //               alt="Logo"
-    //               width={150}
-    //               height={250}
-    //               quality={100}
-    //             />
-    //           </div>
-    //           <div className="title">
-    //             <h1 className="text-3xl text-gray-800 font-semibold mb-2">
-    //               Welcome Back
-    //             </h1>
-    //             <p className="text-gray-500 mx-auto w-full text-xs">
-    //               Welcome back! Please enter your details
-    //             </p>
-    //           </div>
-
-    //           <form className="flex flex-col gap-5">
-    //             <div className="flex flex-col gap-1">
-    //               <label htmlFor="email" className="text-gray-800 text-sm">
-    //                 Email
-    //               </label>
-    //               <div className={styles.input_group}>
-    //                 <input
-    //                   className={styles.input_text}
-    //                   id="email"
-    //                   type="email"
-    //                   name="email"
-    //                   placeholder="Enter your email"
-    //                 />
-    //               </div>
-    //             </div>
-    //             <div className="flex flex-col gap-1">
-    //               <label htmlFor="password" className="text-gray-800 text-sm">
-    //                 Password
-    //               </label>
-    //               <div className={styles.input_group}>
-    //                 <input
-    //                   type={`${showPassword ? "text" : "password"}`}
-    //                   id="password"
-    //                   className={styles.input_text}
-    //                   name="password"
-    //                   placeholder="Enter your password"
-    //                 />
-    //                 <span
-    //                   className="icon flex items-center px-4"
-    //                   onClick={() => setShowPassword(!showPassword)}
-    //                 >
-    //                   {showPassword ? (
-    //                     <FaRegEyeSlash size={20} />
-    //                   ) : (
-    //                     <FaRegEye size={20} />
-    //                   )}
-    //                 </span>
-    //               </div>
-    //             </div>
-
-    //             <div className={styles.input_button}>
-    //               <button className={styles.button} type="submit">
-    //                 Sign in
-    //               </button>
-    //             </div>
-    //             <div className={styles.input_button}>
-    //               <button className={styles.button_custom} type="button">
-    //                 <Image
-    //                   src={"/assets/images/google.svg"}
-    //                   alt="google icon"
-    //                   width={20}
-    //                   height={20}
-    //                 ></Image>
-    //                 Sign in with Google{" "}
-    //               </button>
-    //             </div>
-    //           </form>
-
-    //           <p className="text-center text-sm  text-gray-400">
-    //             Don't have an account?{" "}
-    //             <Link
-    //               href={"/auth/signup"}
-    //               className="text-gray-800 font-medium"
-    //             >
-    //               Sign up for free
-    //             </Link>
-    //           </p>
-    //         </section>
-    //       </div>
-    //     </div>
-    //   </div>
-    //   <div className={styles.backgroundImg + " hidden md:block"}></div>
-    // </div>
   );
 };
 
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,7 +1,141 @@
+"use client";
+import Footer from "@/components/footer/Footer";
+import Link from "next/link";
 import React from "react";
+import { useState } from "react";
+import { FaUser, FaEnvelope, FaLock, FaEye, FaEyeSlash } from "react-icons/fa";
 
 const Signup = () => {
-  return <div>Signup</div>;
+  const [passwordVisible, setPasswordVisible] = useState(false);
+  const [confirmPasswordVisible, setConfirmPasswordVisible] = useState(false);
+
+  const togglePasswordVisibility = () => {
+    setPasswordVisible(!passwordVisible);
+  };
+
+  const toggleConfirmPasswordVisibility = () => {
+    setConfirmPasswordVisible(!confirmPasswordVisible);
+  };
+  return (
+    <div className="h-screen">
+      <div className=" h-full">
+        <div className=" h-full px-40  dark:bg-gray-900 flex items-center justify-center text-center">
+          <div className="  h-full w-1/3 flex flex-col items-center px-10 justify-center gap-8 ">
+            <div className="text-center">
+              <div className="font-vietnam text-h2 font-bold mb-3 whitespace-nowrap">
+                Sign up to our community
+              </div>
+              <div className="text-paragraph text-black dark:text-gray-300 opacity-50">
+                Welcome back, Please enter your details
+              </div>
+            </div>
+            <button className=" flex w-3/4 items-center justify-center gap-4 px-4 py-2 rounded-[12px] font-medium transition-colors dark:bg-gray-900 dark:text-white dark:hover:bg-gray-800 bg-white text-black hover:bg-gray-100 border border-gray-300">
+              <img
+                src="https://www.svgrepo.com/show/355037/google.svg"
+                alt="Google Icon"
+                className="h-5 w-5"
+              />
+              Sign up with Google
+            </button>
+            <div className="flex w-full  items-center justify-between">
+              <div className="border-t w-2/4 border-black dark:border-gray-300 border-opacity-50"></div>
+              <div className="w-full text-sm text-black dark:text-gray-300 opacity-50">
+                or continue with your details
+              </div>
+              <div className="border-t w-2/4 border-black dark:border-gray-300 border-opacity-50"></div>
+            </div>
+            <form className="w-3/4">
+              {/* Full Name Field */}
+              <div className="mb-4">
+                <div className="flex items-center border rounded-[12px] shadow-sm py-2 px-4 gap-3 bg-gray-100 dark:bg-gray-700">
+                  <FaUser className="text-gray-600 dark:text-gray-400 mr-2" />
+                  <input
+                    className="w-full bg-transparent focus:outline-none text-gray-800 dark:text-white"
+                    type="text"
+                    id="fullName"
+                    placeholder="Enter your full name"
+                    required
+                  />
+                </div>
+              </div>
+              {/* Email Field */}
+              <div className="mb-4">
+                <div className="flex items-center border rounded-[12px] shadow-sm py-2 px-4 gap-3 bg-gray-100 dark:bg-gray-700">
+                  <FaEnvelope className="text-gray-600 dark:text-gray-400 mr-2" />
+                  <input
+                    className="w-full bg-transparent focus:outline-none text-gray-800 dark:text-white"
+                    type="email"
+                    id="email"
+                    placeholder="Enter your email"
+                    required
+                  />
+                </div>
+              </div>
+              {/* Password Field */}
+              <div className="mb-4">
+                <div className="flex items-center border rounded-[12px] shadow-sm py-2 px-4 gap-3 bg-gray-100 dark:bg-gray-700">
+                  <FaLock className="text-gray-600 dark:text-gray-400 mr-2" />
+                  <input
+                    className="w-full bg-transparent focus:outline-none text-gray-800 dark:text-white"
+                    type={passwordVisible ? "text" : "password"}
+                    id="password"
+                    placeholder="Enter your password"
+                    required
+                  />
+                  <button
+                    type="button"
+                    onClick={togglePasswordVisibility}
+                    className="ml-2 text-gray-600 dark:text-gray-400 focus:outline-none"
+                  >
+                    {passwordVisible ? <FaEyeSlash /> : <FaEye />}
+                  </button>
+                </div>
+              </div>
+
+              {/* Confirm Password Field */}
+              <div className="mb-4">
+                <div className="flex items-center border rounded-[12px] shadow-sm py-2 px-4 gap-3 bg-gray-100 dark:bg-gray-700">
+                  <FaLock className="text-gray-600 dark:text-gray-400 mr-2" />
+                  <input
+                    className="w-full bg-transparent focus:outline-none text-gray-800 dark:text-white"
+                    type={confirmPasswordVisible ? "text" : "password"}
+                    id="confirmPassword"
+                    placeholder="Confirm your password"
+                    required
+                  />
+                  <button
+                    type="button"
+                    onClick={toggleConfirmPasswordVisibility}
+                    className="ml-2 text-gray-600 dark:text-gray-400 focus:outline-none"
+                  >
+                    {confirmPasswordVisible ? <FaEyeSlash /> : <FaEye />}
+                  </button>
+                </div>
+              </div>
+              {/* Login Button */}
+              <div className="text-center mb-6">
+                <button
+                  type="submit"
+                  className="w-full bg-[#1784C5] hover:bg-[#18567a] text-white font-bold py-2 px-4 rounded-[12px] focus:outline-none focus:shadow-outline"
+                >
+                  Sign up
+                </button>
+              </div>
+
+              <div>
+                Already have an account ?{" "}
+                <Link href="/signin" className="text-[#1784C5]">
+                  {" "}
+                  Login
+                </Link>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
 };
 
 export default Signup;
