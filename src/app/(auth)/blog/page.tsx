c8005c78e09d1ee4a2fdf35d7a622d5194a3c3a9
zmoussam <zakariamoussammi87@gmail.com>
2024-08-22 16:56:17 +0100

    add blog page

--- src/app/(auth)/blog/page.tsx
+++ src/app/(auth)/blog/page.tsx
@@ -2,13 +2,25 @@
 import BlogList from "@/components/blogList/blogList";
 import Image from "next/image";
 import BloggerCard from "@/components/bloggers/bloggerCard";
+import Footer from "@/components/footer/Footer";
+import NavLink from "@/components/navLink/NavLink";
 
 const BlogsPage = () => {
+  const scrollableStyle: React.CSSProperties = {
+    overflowY: "scroll",
+    scrollbarWidth: "none", // For Firefox
+  };
   return (
-    <div className="container mx-auto mt-20 grid grid-cols-5 ">
+    <div className="container mx-auto mt-20 grid lg:grid-cols-5 grid-cols-1 ">
       <BlogList />
-      <aside className="relative border-l pt-8  dark:border-gray-500 col-span-1 min-h-screen">
-        <div className="w-full sticky bottom-0 px-4">
+      <aside
+        className="relative border-l pt-8  dark:border-gray-500 col-span-1 min-h-screen hidden lg:block"
+        aria-label="Sidebar"
+      >
+        <div
+          className="w-full sticky overflow-scroll top-24 px-4"
+          style={scrollableStyle}
+        >
           <div className="flex flex-col gap-4">
             {/* best blogger  */}
             <div className="flex flex-col gap-5 border-b pb-4 dark:border-gray-500">
@@ -29,22 +41,22 @@ const BlogsPage = () => {
             <div className="flex flex-col gap-5 border-b pb-4 dark:border-gray-500">
               <h5 className="font-bold opacity-50">Recommended category</h5>
               <div className="">
-                <button className="text-sm mr-2 mb-2 text-gray-500 dark:text-gray-400 bg-[#E8E8E8] py-2 px-4 rounded-2xl">
+                <button className="text-sm mr-2 mb-2 text-gray-500 dark:text-gray-900 bg-[#E8E8E8] py-2 px-4 rounded-2xl">
                   #Technology
                 </button>
-                <button className="text-sm mr-2 mb-2 text-gray-500 dark:text-gray-400 bg-[#E8E8E8] py-2 px-4 rounded-2xl">
+                <button className="text-sm mr-2 mb-2 text-gray-500 dark:text-gray-900 bg-[#E8E8E8] py-2 px-4 rounded-2xl">
                   #Lifestyle
                 </button>
-                <button className="text-sm mr-2 mb-2 text-gray-500 dark:text-gray-400 bg-[#E8E8E8] py-2 px-4 rounded-2xl">
+                <button className="text-sm mr-2 mb-2 text-gray-500 dark:text-gray-900 bg-[#E8E8E8] py-2 px-4 rounded-2xl">
                   #sport
                 </button>
-                <button className="text-sm mr-2 mb-2 text-gray-500 dark:text-gray-400 bg-[#E8E8E8] py-2 px-4 rounded-2xl">
+                <button className="text-sm mr-2 mb-2 text-gray-500 dark:text-gray-900 bg-[#E8E8E8] py-2 px-4 rounded-2xl">
                   #Food & Drink
                 </button>
-                <button className="text-sm mr-2 mb-2 text-gray-500 dark:text-gray-400 bg-[#E8E8E8] py-2 px-4 rounded-2xl">
+                <button className="text-sm mr-2 mb-2 text-gray-500 dark:text-gray-900 bg-[#E8E8E8] py-2 px-4 rounded-2xl">
                   #Business
                 </button>
-                <button className="text-sm mr-2 mb-2 text-gray-500 dark:text-gray-400 bg-[#E8E8E8] py-2 px-4 rounded-2xl">
+                <button className="text-sm mr-2 mb-2 text-gray-500 dark:text-gray-900 bg-[#E8E8E8] py-2 px-4 rounded-2xl">
                   #Education
                 </button>
               </div>
@@ -56,7 +68,8 @@ const BlogsPage = () => {
                 <div className="flex items-center gap-4">
                   <div className="w-16 h-16 relative">
                     <Image
-                      src="/assets/blogs/blog1.jpg"
+                      alt="blog1"
+                      src="/assets/blogs/blog_1.jpg"
                       layout="fill"
                       objectFit="cover"
                       className="rounded-lg"
@@ -74,7 +87,8 @@ const BlogsPage = () => {
                 <div className="flex items-center gap-4">
                   <div className="w-16 h-16 relative">
                     <Image
-                      src="/assets/blogs/blog2.jpg"
+                      alt=""
+                      src="/assets/blogs/blog_2.jpg"
                       layout="fill"
                       objectFit="cover"
                       className="rounded-lg"
@@ -92,7 +106,8 @@ const BlogsPage = () => {
                 <div className="flex items-center gap-4">
                   <div className="w-16 h-16 relative">
                     <Image
-                      src="/assets/blogs/blog3.jpg"
+                      alt="blog1"
+                      src="/assets/blogs/blog_3.jpg"
                       layout="fill"
                       objectFit="cover"
                       className="rounded-lg"
@@ -108,6 +123,15 @@ const BlogsPage = () => {
                   </div>
                 </div>
               </div>
+            </div>
+
+            <div className="dark:text-gray-200 text-xs grid grid-rows-2 grid-cols-4 gap-4 px-4">
+              <NavLink href="/" content="Home" />
+              <NavLink href="/blogs" content="Blogs" />
+              <NavLink href="/about" content="About" />
+              <NavLink href="/contact" content="Contact" />
+              <NavLink href="/privacy" content="Privacy" />
+              <NavLink href="/signin" content="Sign in" />
             </div>
           </div>
         </div>
