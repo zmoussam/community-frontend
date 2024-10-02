b4a418dd90a63fcd832dc83d46ef0d2942d308ff
Mehdi Akacem <96568645+mehdiakacem@users.noreply.github.com>
2024-10-02 18:32:14 +0100

    added story editor

--- src/app/(auth)/story/[id]/page.tsx
+++ src/app/(auth)/story/[id]/page.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import NewStory from '../NewStory'
+import Navbar from '@/components/navbar/Navbar'
 
 export default function page({params}: {params: {id: string}}) {
 
 	console.log("story id: ", params.id)
   return (
 	<div className='max-w-[1000px] mx-auto'>
+		<Navbar />
 		<NewStory />
 	</div>
   )
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,38 +1,41 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  darkMode: 'class',
+  darkMode: "class",
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    container: { 
+    container: {
       center: true,
     },
     extend: {
       backgroundImage: {
-        'home-background': "url('/assets/background/Home-background.webp')",
+        "home-background": "url('/assets/background/Home-background.webp')",
       },
       fontFamily: {
         vietnam: ["Be Vietnam Pro", "sans-serif"],
       },
       fontSize: {
-        'h1': '48px',
-        'h2': '38px',
-        'h3': '24px',
-        'h4': '20px',
-        'paragraph': '16px',
+        h1: "48px",
+        h2: "38px",
+        h3: "24px",
+        h4: "20px",
+        paragraph: "16px",
       },
       colors: {
-        bg: '#ffffff',
-        textColor: '#000000',
-        softBg: '#f0f0f0',
-        softTextColor: '#626262'
+        bg: "#ffffff",
+        textColor: "#000000",
+        softBg: "#f0f0f0",
+        softTextColor: "#626262",
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require("@tailwindcss/typography"),
+
+  ],
 };
 export default config;
